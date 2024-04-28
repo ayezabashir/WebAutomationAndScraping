882d3f4352dd8ed6983928f9b00b88190710d82d
Ayeza <ayezabashir442@gmail.com>
2024-04-28 22:21:01 +0500

    opening the website

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import service
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
+                          options=options)
+
+driver.get("https://neuralnine.com/")
+driver.maximize_window()
+
