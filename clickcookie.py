9c29894391fd6190875386b046b2c7cb0823c7df
Ayeza <ayezabashir442@gmail.com>
2024-04-29 18:17:06 +0500

    cookies

--- clickcookie.py
+++ clickcookie.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
@@ -46,6 +45,3 @@ while True:
             product.click()
             break
 
-time.sleep(60)
-
-driver.quit()
